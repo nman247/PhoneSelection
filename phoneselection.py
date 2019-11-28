39341e4a541132041ded437dacee196810d1eb14
nman247 <57647876+nman247@users.noreply.github.com>
2019-11-27 19:36:27 -0500

    Update phoneselection.py

--- phoneselection.py
+++ phoneselection.py
@@ -1,1 +1,11 @@
+import math
+def funcchoice():
+    inhouse = str(input("Hey Are You Buying A New Phone? Answer Yes or No "))
+    return inhouse
+def funcown():
+    owns = str(input("Do you own any of the phones that you are adding?"))
+    return owns
+def funcnumofline():
+    numOflines = int(input("How many Lines are you planning to add?  "))
+    return numOflines
 
